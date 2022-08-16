a6354daf6637fd815c18ae4313647f5c14f287ea
emersonqaacademy <emerson.qaacademy@gmail.com>
2022-08-16 19:15:04 -0300

    add tests exercises 4 and 5

---
+++ src/main/java/medium/Exercicio_05_medium.java
@@ -0,0 +1,22 @@
+package medium;
+
+import javax.swing.*;
+
+public class Exercicio_05_medium {
+
+    static int numeroDigitado, i, menorNumero;
+    public static void main(String[] args) {
+        while(i < 5){
+            numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+            if (i == 0){
+                menorNumero = numeroDigitado;
+            }
+            if (numeroDigitado < menorNumero){
+                menorNumero = numeroDigitado;
+            }
+            i++;
+        }
+        System.out.println("O menor número é: " + menorNumero);
+
+    }
+}
