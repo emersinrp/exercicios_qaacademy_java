de0263ef3affc08893428b7a22762dfe0a6894e7
emersonqaacademy <emerson.qaacademy@gmail.com>
2022-09-01 19:13:09 -0300

    add exercises methods + parameters

--- src/main/java/easy/Exercicios_easy.java
+++ src/main/java/easy/Exercicios_easy.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Exercicios_easy {
 
-    public static void exercicio_02_easy(){
+    public void exercicio_02_easy(){
         String palavraDigitada;
         palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra:");
         System.out.println("O usuario digitou: " + palavraDigitada);
